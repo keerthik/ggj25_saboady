3c7485f7376510faebed4dde157f64cf3707ed98
keerthik <keerthiko@gmail.com>
2025-01-25 15:33:03 -0500

    adding prefabs folder, director prefab

--- saboady/Assets/Scripts/GameDirector.cs
+++ saboady/Assets/Scripts/GameDirector.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour
 {
+    [SerializeField] private LoadingSystem.SCENE firstScene;
+
     void Awake() {
         DontDestroyOnLoad(gameObject);
     }
 
     void Start() {
         // Let's load the main menu and show it when it's ready.
-        LoadingSystem.Shared.LoadSceneAndThen(LoadingSystem.SCENE.MAINMENU, () => {
+        LoadingSystem.Shared.LoadSceneAndThen(firstScene, () => {
             // Do a camera fade or something if you want
             LoadingSystem.Shared.MakeCurrentSceneActive();
         });
