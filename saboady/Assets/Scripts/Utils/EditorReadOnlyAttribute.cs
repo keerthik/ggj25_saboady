7c7d48d58a923cb6ab3504ba81c274b5c49633fb
keerthik <keerthiko@gmail.com>
2025-01-25 07:40:41 -0500

    adding some generic utils

---
+++ saboady/Assets/Scripts/Utils/EditorReadOnlyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+ 
+/// <summary>
+/// Allow to display an attribute in inspector without allow editing
+/// </summary>
+public class EditorReadOnlyAttribute : PropertyAttribute {
+ 
+    public EditorReadOnlyAttribute()
+    {
+ 
+    }
+}
---
+++ saboady/Assets/Scripts/Utils/SingletonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Subclass this for globally active and accessed singleton behaviours.
+ eg: TourDownloadManager: SingletonBehaviour<TourDownloadManager>
+ NOTE: Use <WorkoutAssistant> for singletons meant to be used exclusively within a workout
+*/
+public class SingletonBehaviour : MonoBehaviour
+{
+}
+
+public abstract class SingletonBehaviour<SingletonAssistant> : SingletonBehaviour
+    where SingletonAssistant : SingletonBehaviour<SingletonAssistant> {
+    private static SingletonAssistant instance = null;
+    public static SingletonAssistant Shared {
+        private set { instance = value; }
+        get { return instance; }
+    }
+
+    // Because we want assignments to be done first, call this at the end of overriding 'new' Awake
+    protected virtual void Awake() {
+        instance = (SingletonAssistant)this;
+    }
+}
