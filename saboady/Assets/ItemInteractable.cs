1e48ded8ad2eda2a3492291965d0366f88327419
keerthik <keerthiko@gmail.com>
2025-01-26 12:21:24 -0500

    updating prefab with some strings, using player_test for complex interactions

--- saboady/Assets/ItemInteractable.cs
+++ saboady/Assets/ItemInteractable.cs
@@ -2,14 +2,35 @@ using UnityEngine;
 
 public class ItemInteractable : Interactable
 {
+    [SerializeField] private int targetProgress;
+    [SerializeField] private string itemKey;
+
     public override (string, string) GetDialog() {
+        int trackProgress = goodTrack? GameDirector.Shared.good : GameDirector.Shared.bad;
         if (GameDirector.Shared.good == 1) {
-            return (Constructors.Shared.water_questdone_1, "");
+            return (Items.Shared.all[itemKey], "");
         } else {
             // isTrigger = false;
             // Make sure doesn't disable too soon
         }
         // We should not have gotten here!
         return (null, null);
+    }
+
+    // Items are non-interactable at the start
+    protected override void Awake() {
+        GetComponent<Collider>().isTrigger = false;
+    }
+
+    void Update() {
+
+    }
+    public override void DismissAction() {
+        if (goodTrack) {
+            GameDirector.Shared.UpGoodProgress(targetProgress);
+        } else {
+            GameDirector.Shared.UpBadProgress(targetProgress);
+        }
+        gameObject.SetActive(false);
     }
 }
--- saboady/Assets/PondNPCInteractable.cs
+++ saboady/Assets/PondNPCInteractable.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class PondNPCInteractable : Interactable
 {
     public override (string, string) GetDialog() {
-        if (GameDirector.Shared.good < 1) {
+        if (TrackProgress < 1) {
             return (Constructors.Shared.pond["contact_0"], "unsure");
-        } else if (GameDirector.Shared.good == 1) {
+        } else if (TrackProgress == 1) {
             return (Constructors.Shared.pond["midquest"], "illlook");
-        } else {
-            // isTrigger = false;
         }
-        // We should not have gotten here!
+        // We have nothing to do with this guy
         return (Basics.Shared.nothing, "moveon");
     }
     public override void DismissAction() {
-        if (GameDirector.Shared.good < 2) {
+        if (TrackProgress < 2) {
             GameDirector.Shared.UpGoodProgress(1);
         } else {
             GameDirector.Shared.UpGoodProgress(2);
