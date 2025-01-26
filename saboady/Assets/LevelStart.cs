e469fdd4f55f1d111899b9b73db99a5a0a2a5e22
keerthik <keerthiko@gmail.com>
2025-01-26 14:24:52 -0500

    playercharacter resize

--- saboady/Assets/LevelStart.cs
+++ saboady/Assets/LevelStart.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 public class LevelStart : MonoBehaviour
 {
     [SerializeField] private GameObject playerControllerPrefab;
-    [SerializeField] private GameObject playerCameraPrefab;
     void Start()
     {
         Instantiate(playerControllerPrefab, transform.position, transform.rotation);
-        // Instantiate(playerCameraPrefab, transform.position, transform.rotation); 
     }
 }
