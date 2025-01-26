25fbc60e2f906bbe9a443cb65f6e357011799b9a
keerthik <keerthiko@gmail.com>
2025-01-25 20:48:12 -0500

    interaction system

--- saboady/Assets/Scripts/Interactable.cs
+++ saboady/Assets/Scripts/Interactable.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
     // Override this for each interactable in the game
-    public (string, string) GetDialog(PlayerProgression progression) {
+    public virtual (string, string) GetDialog(PlayerProgression progress) {
         return (ConstructorStrings.Shared.pond_midquest, 
             PlayerStrings.Shared.move_on);
+    }
+
+    public virtual void DismissAction(PlayerProgression progress) {
+        
     }
 }
